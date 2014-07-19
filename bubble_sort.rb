6c410a394a68ffbccdcb37a664cf8980729782c1
sandiegodj <davidjanczyn@gmail.com>
2014-07-19 15:22:15 -0700

    all enumerable methods working properly

---
+++ bubble_sort.rb
@@ -0,0 +1,37 @@
+/def bubble_sort(array = [55,488,32,11,2])
+sorted = false
+until sorted
+sorted = true
+	array.each_with_index do |element_1, index|
+		element_2 = array[index + 1]
+		if (element_1 <=> element_2) == 1
+			array[index], array[index + 1] = element_2, element_1
+			sorted = false			
+		end
+	end
+
+end
+puts array
+end
+
+bubble_sort/
+
+
+def bubble_sort_by(array)
+	sorted = false
+	until sorted
+		sorted = true
+		array.each_with_index do |element_1, index|
+			element_2 = array[index + 1]
+			if yield(element_1.to_s, element_2.to_s) == -1
+				array[index], array[index + 1] = element_2, element_1
+				sorted = false
+			end
+		end
+	end
+	puts array
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+       right.length <=> left.length
+     end
---
+++ enumerable_methods.rb
@@ -0,0 +1,91 @@
+class Array
+	def my_each
+		i = 0
+		while i < self.size
+			yield(self[i])
+			i+=1
+		end
+		self
+	end
+
+	def my_each_with_index
+		i = 0
+		while i < self.size
+			yield(self[i], i)
+			i+=1
+		end
+		self
+	end
+
+	def my_select
+		result = []
+	    self.my_each {|i| result << i if yield(i)}
+	    result
+	end
+
+	def my_all?
+		self.my_select { |i| yield(i) } == self ? true : false
+	end
+
+	def my_any? 
+		self.my_select {|i| yield(i)}.size > 0 ? true : false 
+	end
+
+	def my_none?
+		self.my_select {|i| yield(i)}.size > 0 ? false : true 
+	end
+
+	def my_count
+		i = 0
+		result = 0
+		while i < self.size
+			if block_given?
+				if yield(self[i]) 
+					i += 1
+					result += 1
+				else
+					i += 1
+				end
+			else
+				i += 1
+				result += 1
+			end
+		end
+		puts result
+	end
+
+	def my_map
+		result = []
+		i = 0
+		while i < self.size
+			if block_given?
+				result << yield(self[i])
+				i += 1
+			else
+				return self
+			end
+		end
+		puts result
+	end
+
+	def my_inject(total = self[0])
+		i = 0
+		while i < self.size
+			total = yield(total, self[i])
+			i += 1
+		end
+		total
+
+		# after each iterations, the result of the block is added to total
+
+	end
+
+
+
+end
+def multiply_els(array = [2,4,5])
+	answer = array.my_inject(1) {|total, n| total * n}
+	puts answer
+end
+
+
